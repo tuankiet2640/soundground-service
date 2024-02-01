89180eab4d6898fb31d1e3b2a9d29d59e9d95589
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2024-02-01 11:21:42 +0700

    create project
    first commit

---
+++ vietkast/src/main/java/com/example/vietkast/VietkastApplication.java
@@ -0,0 +1,13 @@
+package com.example.vietkast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietkastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VietkastApplication.class, args);
+	}
+
+}
